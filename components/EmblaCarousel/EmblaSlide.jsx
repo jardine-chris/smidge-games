7b1c1c31b0f49f3e4014584f92c6a477de88f989
Chris Jardine <c_jardine@icloud.com>
2022-01-27 22:17:39 -0500

    Fix font size, line height, and padding for carousel responsiveness.

--- components/EmblaCarousel/EmblaSlide.jsx
+++ components/EmblaCarousel/EmblaSlide.jsx
@@ -1,13 +1,13 @@
 export const EmblaSlide = (props) => {
   return (
     <div className="embla__slide">
-      <div className="grid grid-cols-1 embla__slide__inner lg:grid-cols-2">
-        <img className="w-full" src={props.imgSrc} alt={props.imgAlt} />
-        <div className="px-6 pt-4">
-          <h1 className="text-xl font-bold text-white font-lato">
+      <div className="grid grid-cols-1 embla__slide__inner lg:grid-cols-2 xl:grid-cols-3">
+        <img className="xl:col-span-2" src={props.imgSrc} alt={props.imgAlt} />
+        <div className="px-6 pt-4 md:px-16">
+          <h1 className="text-xl font-bold text-zinc-200 font-lato">
             {props.title}
           </h1>
-          <p className="mt-2 text-sm tracking-tight text-zinc-400 font-montserrat">
+          <p className="mt-2 font-light leading-relaxed tracking-tight text-md md:text-base xl:text-lg xl:leading-normal text-zinc-400 font-montserrat">
             {props.content}
           </p>
         </div>

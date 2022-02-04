02c11a4e97b00b9124c1e9e3567f3e45fb24fe84
Chris Jardine <c_jardine@icloud.com>
2022-02-04 13:38:24 -0500

    Add loading bar on route changes.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import { Navbar } from "../components/Navbar/Navbar";
 import "../styles/globals.scss";
+import Router from "next/router";
+import NProgress from "nprogress";
+import "../styles/nprogress.css";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
 
   return (
     <div className="bg-black">
+      
       <div className="border-b border-grurp-800">
         <EmblaCarouselDots>
           <EmblaSlide

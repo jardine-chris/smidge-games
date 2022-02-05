6633227ff328b0802b1d84bd202fc9e280dfa156
Chris Jardine <c_jardine@icloud.com>
2022-02-05 14:14:18 -0500

    Add pagination to load more post cards on the home page.

--- components/Card/CardSection.jsx
+++ components/Card/CardSection.jsx
@@ -1,15 +1,37 @@
 import { useState, useEffect } from "react";
 import { Card } from "./Card";
 import axios from "axios";
-import useSWR from "swr";
 import { getDate } from "../../utils/datetime";
+import { ImSpinner9, ImArrowDown2 } from "react-icons/im";
 
 export const CardSection = () => {
-  const allPostsUrl = "https://api.smidgegames.com/wp-json/wp/v2/posts?per_page=4";
-  const fetcher = async (url) => await axios.get(url).then((res) => res.data);
-  const { data, error } = useSWR(allPostsUrl, fetcher);
-  if (error) <p>Loading failed...</p>;
-  if (!data) <h1>loading...</h1>;
+  const [totalPages, setTotalPages] = useState(1);
+  const [page, setPage] = useState(1);
+  const [postData, setPostData] = useState([]);
+  const [isFetching, setIsFetching] = useState(true);
+  const [hasMore, setHasMore] = useState(true);
+
+  const getPosts = async () => {
+    setIsFetching(true);
+    axios(`https://api.smidgegames.com/wp-json/wp/v2/posts`, {
+      params: {
+        per_page: 1,
+        page: page,
+      },
+    }).then((res) => {
+      console.log(`Headers: ${res.headers["x-wp-total"]}`);
+      setTotalPages(res.headers["X-WP-TotalPages"]);
+      setPostData([...postData, ...res.data]);
+      setPage((prevPage) => prevPage + 1);
+      setHasMore(page <= totalPages);
+
+      setIsFetching(false);
+    });
+  };
+
+  useEffect(async () => {
+    getPosts();
+  }, []);
 
   return (
     <div>
@@ -17,8 +39,8 @@ export const CardSection = () => {
         Latest News
       </h1>
       <div className="grid grid-cols-1 px-2 pb-2 gap-y-6 sm:px-8 sm:grid-cols-2 sm:gap-8 lg:grid-cols-3 xl:grid-cols-4">
-        {data &&
-          data.map((post) => (
+        {postData &&
+          postData.map((post) => (
             <Card
               key={post.id}
               featuredImageUrl={post._links["wp:featuredmedia"][0].href}
@@ -32,6 +54,26 @@ export const CardSection = () => {
               id={post.id}
             />
           ))}
+      </div>
+      <div className="px-2">
+        {isFetching && (
+          <button
+            disabled
+            className="w-full py-2 text-gray-400 rounded bg-grurp-700"
+          >
+            <ImSpinner9 className="inline text-orange-600 animate-spin" />
+          </button>
+        )}
+        {!isFetching && hasMore && (
+          <div className="mt-4 text-center">
+            <button
+              className="relative w-12 h-12 py-2 text-white bg-orange-600 rounded-full animate-bounce"
+              onClick={getPosts}
+            >
+              <ImArrowDown2 className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" />
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
--- pages/posts/latest-posts.js
+++ pages/posts/latest-posts.js
@@ -1,3 +1,17 @@
+import useSWR from "swr";
+
 export default function LatestPosts() {
-    return <h1>Latest Posts</h1>
+  const allPostsUrl =
+    "https://api.smidgegames.com/wp-json/wp/v2/posts?per_page=4";
+  const fetcher = async (url) => await axios.get(url).then((res) => res.data);
+  const { data, error } = useSWR(allPostsUrl, fetcher);
+  if (error) <p>Loading failed...</p>;
+  if (!data) <h1>loading...</h1>;
+  return (
+    <div>
+      {data.map((post) => {
+        return <h1>{post.title.rendered}</h1>;
+      })}
+    </div>
+  );
 }

076f537255b13a5149b4370d92d84f4555f8a14b
Chris Jardine <c_jardine@icloud.com>
2022-02-06 16:00:38 -0500

    Fix bottom padding of the news section on home page.

--- components/Card/CardSection.jsx
+++ components/Card/CardSection.jsx
@@ -55,7 +55,7 @@ export const CardSection = () => {
             />
           ))}
       </div>
-      <div className="mb-2">
+      <div className="pb-16">
         {isFetching && (
           <button
             disabled

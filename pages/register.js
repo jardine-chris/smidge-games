dbdfbcb39fc8fd1c21eaa68c80c03722c5b13d49
Chris Jardine <c_jardine@icloud.com>
2022-02-06 15:45:35 -0500

    Add remaining form fields on the registration page and add background scroll effect.

--- pages/register.js
+++ pages/register.js
@@ -1,85 +1,92 @@
 import { FloatingInput } from "../components/Form/FloatingInput";
 
 export default function Register() {
-
   return (
     <div className="background">
-      <div class="py-16 w-full mx-auto max-w-lg">
-      <form class="bg-zinc-800 border border-zinc-600/70 shadow-md font-montserrat rounded px-4 pt-6 pb-8 mb-4">
-        <FloatingInput id="email" type="email" label="Email" />
-        </form>
-        {/* <form class="bg-black border border-zinc-600/70 shadow-md font-montserrat rounded px-4 pt-6 pb-8 mb-4">
-          <div class="mb-4">
-            <label
-              class="block text-white uppercase text-sm font-bold mb-2"
-              for="username"
-            >
-              Username
-            </label>
-            <input class={inputBoxStyle} id="username" type="text" />
+      <div className="w-full max-w-lg py-16 mx-auto">
+        <form className="px-4 pt-6 pb-8 mb-4 border rounded shadow-md bg-zinc-800 border-zinc-600/70 font-montserrat">
+          <h1 className="mb-4 font-bold text-white uppercase">
+            Create an Account
+          </h1>
+          <div className="grid grid-cols-2 gap-4">
+            <FloatingInput id="first_name" type="text" label="First Name" />
+            <FloatingInput id="last_name" type="text" label="Last Name" />
           </div>
-          <div class="mb-4">
-            <label
-              class="block text-white uppercase text-sm font-bold mb-2"
-              for="email"
-            >
-              Email
-            </label>
-            <input class={inputBoxStyle} id="email" type="email" />
-          </div>
-          <div class="mb-6">
-            <label
-              class="block text-white uppercase text-sm font-bold mb-2"
-              for="password"
-            >
-              Password
-            </label>
-            <input
-              class={inputBoxStyle}
-              id="password"
-              type="password"
-              placeholder="******************"
-            />
-          </div>
-          <div class="flex items-center justify-between">
-            <button
-              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
-            >
-              Sign In
+          <FloatingInput id="username" type="text" label="Username" />
+          <FloatingInput id="email" type="email" label="Email" />
+          <FloatingInput id="password" type="password" label="Password" />
+          <div className="grid grid-cols-2">
+            <button className="px-8 py-2 font-bold text-white uppercase bg-black hover:bg-orange-600">
+              Register
             </button>
-            <a
-              class="inline-block align-baseline font-bold text-sm text-orange-600 hover:text-white"
-              href="#"
-            >
-              Forgot Password?
-            </a>
+            <h1 className="self-center font-bold text-orange-600 uppercase cursor-pointer hover:text-white justify-self-end">Forgot password?</h1>
           </div>
-        </form> */}
+        </form>
       </div>
       <style jsx>
-          {`
+        {`
           .background {
-              position: relative;              
+            position: relative;
           }
 
           .background::before {
-              content: "";
-              position: absolute;
-              top: 0;
-              left: 0;
-              width: 100%;
-              height: 100%;
-              background: linear-gradient(to right, rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)), url("/images/smidge-games-logo/logo-tessellate-weave.png");
-              background-repeat: repeat;
-              background-size: 12rem;
-              transform: scale(1.1);
-              filter: blur(1px);
-              z-index: -1;
-              overflow: hidden;
-
+            content: "";
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background: linear-gradient(
+                to right,
+                rgba(0, 0, 0, 0.75),
+                rgba(0, 0, 0, 0.75)
+              ),
+              url("/images/smidge-games-logo/logo-tessellate-weave.png");
+            background-repeat: repeat;
+            background-size: 12rem;
+            transform: scale(1.1);
+            filter: blur(1px);
+            z-index: -1;
+            overflow: hidden;
+            -webkit-animation: animate_background 10s linear 0s infinite;
+            -moz-animation: animate_background 10s linear 0s infinite;
+            -o-animation: animate_background 10s linear 0s infinite;
+            animation: animate_background 10s linear 0s infinite;
           }
-          `}
+
+          @-webkit-keyframes animate_background { 
+            from { 
+                background-position: 0 0;
+            } 
+            to { 
+                background-position: -200px 0;
+            } 
+        }
+        @-moz-keyframes animate_background { 
+            from { 
+                background-position: 0 0;
+            } 
+            to { 
+                background-position: -200px 0;
+            } 
+        }
+        @-o-keyframes animate_background { 
+            from { 
+                background-position: 0 0;
+            } 
+            to { 
+                background-position: -200px 0;
+            } 
+        }
+        @keyframes animate_background { 
+            from { 
+                background-position: 0 0;
+            } 
+            to { 
+                background-position: -200px 0;
+            } 
+        }
+        `}
       </style>
     </div>
   );
